d3ea49fc9f5e9e0a7899c1dfb1b12b7a08ba90d5
ue-sho <tssu45@gmail.com>
2021-01-13 08:19:47 +0900

    Reactの導入

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,35 @@ const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
-  entry: './src/app.js',
+  entry: './src/app.tsx',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
   },
   module: {
     rules: [
       {
-        test: /\.(j|t)s$/,  // js or ts
+        test: /\.(j|t)sx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
           }
         }
       }
     ]
   },
-}
+  devServer: {
+    contentBase: __dirname,
+    port: 5000
+  },
+};
